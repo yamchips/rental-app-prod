fcc0bb531e92973a1fafeda48b161b6046fb50eb
yamchips <wflunch@gmail.com>
2025-07-05 13:48:30 +0100

    Create Footer section

--- src/app/(nonedashboard)/landing/page.tsx
+++ src/app/(nonedashboard)/landing/page.tsx
@@ -1,6 +1,7 @@
 import CallToActionSection from "./CallToActionSection";
 import DiscoverSection from "./DiscoverSection";
 import FeaturesSection from "./FeaturesSection";
+import FooterSection from "./FooterSection";
 import HeroSection from "./HeroSection";
 
 const Landing = () => {
@@ -10,6 +11,7 @@ const Landing = () => {
       <FeaturesSection />
       <DiscoverSection />
       <CallToActionSection />
+      <FooterSection />
     </div>
   );
 };
