69d5524acb1d694c1f04f3ff4c76b4eac026ab15
yamchips <wflunch@gmail.com>
2025-07-31 16:22:34 +0100

    Finish tenant favorite page

--- client/src/app/(dashboard)/tenants/favorites/page.tsx
+++ client/src/app/(dashboard)/tenants/favorites/page.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+"use client";
+import Card from "@/components/Card";
+import Header from "@/components/Header";
+import Loading from "@/components/Loading";
+import {
+  useGetAuthUserQuery,
+  useGetPropertiesQuery,
+  useGetTenantQuery,
+} from "@/state/api";
+import { Property } from "@/types/prismaTypes";
 
 const Favorites = () => {
-  return <div>Favorites</div>;
+  const { data: authUser } = useGetAuthUserQuery();
+  const { data: tenant } = useGetTenantQuery(
+    authUser?.cognitoInfo?.userId || "",
+    { skip: !authUser?.cognitoInfo?.userId }
+  );
+  const {
+    data: favoriteProperties,
+    isLoading,
+    error,
+  } = useGetPropertiesQuery(
+    { favoriteIds: tenant?.favorites?.map((fav: Property) => fav.id) },
+    { skip: !tenant?.favorites || tenant?.favorites.length === 0 }
+  );
+
+  if (isLoading) return <Loading />;
+  if (error) return <div>Error loading favorites</div>;
+
+  return (
+    <div className="dashboard-container">
+      <Header
+        title={"Favorited Properties"}
+        subtitle={"Browse and manage your saved property"}
+      />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        {favoriteProperties?.map((property) => (
+          <Card
+            key={property.id}
+            property={property}
+            isFavorite={true}
+            onFavoriteToggle={() => {}}
+            showFavoriteButton={false}
+            propertyLink={`/tenants/residences/${property.id}`}
+          />
+        ))}
+      </div>
+      {(!favoriteProperties || favoriteProperties.length === 0) && (
+        <p>You don&lsquo;t have any favorited properties</p>
+      )}
+    </div>
+  );
 };
 
 export default Favorites;
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = ({ title, subtitle }: HeaderProps) => {
+  return (
+    <div className="mb-5">
+      <h1 className="text-xl font-semibold">{title}</h1>
+      <p className="text-sm text-gray-500 mt-1">{subtitle}</p>
+    </div>
+  );
+};
+
+export default Header;
