1be03fa22cdf69f48d29394d570c6e8dbe3d0195
yamchips <wflunch@gmail.com>
2025-08-05 16:46:26 +0100

    Improve search in hero section

--- client/src/app/(nonedashboard)/landing/HeroSection.tsx
+++ client/src/app/(nonedashboard)/landing/HeroSection.tsx
@@ -2,11 +2,52 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { setFilters } from "@/state";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 
 const HeroSection = () => {
+  const dispatch = useDispatch();
+  const [searchQuery, setSearchQuery] = useState("");
+  const router = useRouter();
+
+  const handleLocationSearch = async () => {
+    try {
+      const trimmedQuery = searchQuery.trim();
+      if (!trimmedQuery) return;
+
+      const response = await fetch(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+          trimmedQuery
+        )}.json?access_token=${
+          process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+        }&fuzzyMatch=true`
+      );
+
+      const data = await response.json();
+      if (data.features && data.features.length > 0) {
+        const [lng, lat] = data.features[0].center;
+        dispatch(
+          setFilters({
+            location: trimmedQuery,
+            coordinates: [lng, lat],
+          })
+        );
+        const params = new URLSearchParams({
+          location: trimmedQuery,
+          lat: lat.toString(),
+          lng: lng.toString(),
+        });
+        router.push(`/search?${params.toString()}`);
+      }
+    } catch (error) {
+      console.error("error search location:", error);
+    }
+  };
+
   return (
     <div className="relative h-screen">
       <Image
@@ -34,13 +75,15 @@ const HeroSection = () => {
           <div className="flex justify-center">
             <Input
               type="text"
-              value="search query"
-              onChange={() => {}}
+              value={searchQuery}
+              onChange={(e) => {
+                setSearchQuery(e.target.value);
+              }}
               placeholder="Search by city, neighborhood or address"
               className="max-w-lg rounded-none rounded-l-xl border-none bg-white h-12"
             />
             <Button
-              onClick={() => {}}
+              onClick={handleLocationSearch}
               className="bg-secondary-500 text-white rounded-none rounded-r-xl border-none hover:bg-secondary-600 h-12"
             >
               Search
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,13 @@
+import Navbar from "@/components/Navbar";
+import Landing from "./(nonedashboard)/landing/page";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="w-full h-full">
+      <Navbar />
+      <main className={`h-full flex w-full flex-col`}>
+        <Landing />
+      </main>
+    </div>
+  );
 }
