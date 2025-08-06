cdbeebce17cc5ab9c45f792e1e5b59ca30ebb8f8
yamchips <wflunch@gmail.com>
2025-08-06 10:53:41 +0100

    Prepare deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -35,7 +35,7 @@ app.use("/tenants", authMiddleware(["tenant"]), tenantRoutes);
 app.use("/managers", authMiddleware(["manager"]), managerRoutes);
 
 // SERVER
-const port = process.env.PORT || 3002;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3002;
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
