42838a4aed500911ea6a373e8f61fb43c3504a52
yamchips <wflunch@gmail.com>
2025-08-22 20:47:39 +0100

    Improve single property page

--- client/src/app/(nonedashboard)/search/[id]/page.tsx
+++ client/src/app/(nonedashboard)/search/[id]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useGetAuthUserQuery } from "@/state/api";
+import { useGetAuthUserQuery, useGetPropertyQuery } from "@/state/api";
 import { useParams } from "next/navigation";
 import ImagePreviews from "./ImagePreviews";
 import PropertyOverview from "./PropertyOverview";
@@ -8,17 +8,25 @@ import PropertyLocation from "./PropertyLocation";
 import ContactWidget from "./ContactWidget";
 import ApplicationModal from "./ApplicationModal";
 import { useState } from "react";
+import Loading from "@/components/Loading";
 
 const SingleListing = () => {
   const { id } = useParams();
   const propertyId = Number(id);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const { data: authUser } = useGetAuthUserQuery();
-
+  const { data: property, isLoading: propertyLoading } =
+    useGetPropertyQuery(propertyId);
+  const propertyPhoto = property?.photoUrls;
+  if (propertyLoading) return <Loading />;
   return (
     <div>
       <ImagePreviews
-        images={["/singlelisting-2.jpg", "/singlelisting-3.jpg"]}
+        images={
+          propertyPhoto
+            ? propertyPhoto
+            : ["/singlelisting-2.jpg", "/singlelisting-3.jpg"]
+        }
       />
       <div className="flex flex-col md:flex-row justify-center gap-10 mx-10 md:w-2/3 md:mx-auto mt-16 mb-8">
         <div className="order-2 md:order-1">
