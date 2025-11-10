62cf5d23a36578fe4c908b93b90d93fbdf12d2f2
yamchips <wflunch@gmail.com>
2025-11-10 22:35:27 +0000

    Add default city to hero section

--- client/src/app/(nonedashboard)/landing/HeroSection.tsx
+++ client/src/app/(nonedashboard)/landing/HeroSection.tsx
@@ -11,7 +11,7 @@ import { useDispatch } from "react-redux";
 
 const HeroSection = () => {
   const dispatch = useDispatch();
-  const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState("Los Angeles");
   const router = useRouter();
 
   const handleLocationSearch = async () => {
