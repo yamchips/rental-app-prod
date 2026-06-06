fbfe5c695f9fa9575a934e72d2ecca1695f3058a
yamchips <fanwu.it@gmail.com>
2026-06-06 15:37:10 +0100

    Improve single property page

--- client/src/app/(nonedashboard)/search/[id]/page.tsx
+++ client/src/app/(nonedashboard)/search/[id]/page.tsx
@@ -23,9 +23,9 @@ const SingleListing = () => {
     <div>
       <ImagePreviews
         images={
-          propertyPhoto
-            ? propertyPhoto
-            : ["/singlelisting-2.jpg", "/singlelisting-3.jpg"]
+          (propertyPhoto === undefined || propertyPhoto.length === 0) 
+            ? ["/singlelisting-2.jpg", "/singlelisting-3.jpg"]
+            : propertyPhoto
         }
       />
       <div className="flex flex-col md:flex-row justify-center gap-10 mx-10 md:w-2/3 md:mx-auto mt-16 mb-8">
